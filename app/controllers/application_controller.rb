3b8e87269cbdc407830a68f28a6b3452795fd038
Wayne Koler <kolbbd@gmail.com>
2011-08-21 18:51:27 +0400

    getcart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
+  def current_cart
+    Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110821090712) do
+ActiveRecord::Schema.define(:version => 20110821144657) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -18,6 +23,14 @@ ActiveRecord::Schema.define(:version => 20110821090712) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_it"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "cart_id"
   end
 
   create_table "posts", :force => true do |t|
