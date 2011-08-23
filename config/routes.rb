3a3521262fe249a9cad701f725cbd76c097f4182
Wayne Koler <kolbbd@gmail.com>
2011-08-23 23:00:07 +0400

    devise works

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "store#index", :as=> "store"
+   root :to => "store#index"
 
   # See how all your routes lay out with "rake routes"
 
